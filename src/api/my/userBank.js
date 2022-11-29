4b9dd4e4df577c77eb43b600cf9c5e466db4c024
chuanyuan_an <1162734640@qq.com>
2022-11-29 21:26:09 +0800

    我的收益接口/收款信息的绑定

--- src/api/my/userBank.js
+++ src/api/my/userBank.js
@@ -2,9 +2,9 @@ import request from "@/utils/request.js";
 
 /*收款信息*/
 // 收款类型 0:支付宝 1:银行卡 2:微信，不传查全部
-export function userBankList(bankType ) {
+export function userBankList() {
     return request({
-        url: `/shopUserBank/list?bankType=${bankType}`,
+        url: `/shopUserBank/list/`,
         method: 'get',
     })
 }
